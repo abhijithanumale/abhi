4b15545deb70680ded72ed50f17341724534b8fd
abhijithanumale <78299242+abhijithanumale@users.noreply.github.com>
2023-05-12 14:55:40 +0530

    Update trip.java

--- src/main/java/org/example/trip.java
+++ src/main/java/org/example/trip.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class trip {
     public static void main(String[] args) {
-        System.out.println("went to goa");
+        System.out.println("went to Rajasthan");
     }
 }
